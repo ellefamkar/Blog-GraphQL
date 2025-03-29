59589ea2c923c5c9ae7d84784420afe7bcc44b32
Elle <ellefamkar@gmail.com>
2025-03-29 21:47:59 +1100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Graph QL blog</h1>
+    </>
+  )
+}
+
+export default App
+
+// Notes for users:
+// for backend : 
+// GraphCMS -> hygraph.com:  https://hygraph.com/
+// -> create project -> create schema -> create models(posts, writers, comments)
+// -> post -> title + content + cover_photo + date_published + slug
+// ---> add some posts 
+// -> Author -> name + avatar + field + description + slug
+// --> connect authors with posts using Relations (Reference) in GraphCMS
+// -> Comment -> name + email + text +  Reference 
+// Let's build the front
+// Install GraphQL + Apollo/Client
+// : npm install @apollo/client graphql
+// Initialize Graphql using apollo/client in main.jsx
+
+
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://ap-south-1.cdn.hygraph.com/content/cm8t72qeu01sx07uyq47xfku6/master",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+);
